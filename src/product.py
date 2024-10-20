53304593c87a0058ce6b46414bf1fb3148cc6c11
Buzin Dmitriy <89111703879@mail.ru>
2024-10-20 20:24:11 +0300

    fix product, category, utils

--- src/product.py
+++ src/product.py
@@ -1,5 +1,6 @@
 class Product:
-    """Класс для описания товара"""
+    """Класс описывает название продукта, назначение, цену и количество продукта"""
+
     name: str
     description: str
     price: float
@@ -12,21 +13,40 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        """Магический метод возвращающий строковое отображение информации о стоимости и количестве продукта"""
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        """Магический метод возвращает сумму цен двух товаров"""
+        return (self.price * self.quantity) + (other.price * other.quantity)
+
     @classmethod
-    def new_product(cls, dict_product):
-        return cls(dict_product['name'], dict_product['description'], dict_product['price'], dict_product['quantity'])
+    def new_product(cls, dict_product: dict, products=None):
+        """Метод добавляет новый продукт"""
+        if products:
+            for product in products:
+                if product.name == dict_product["name"]:
+                    product.quantity += dict_product["quantity"]
+                    product.price = max([product.price, product["price"]])
+                    return product
+        return cls(**dict_product)
 
     @property
     def price(self):
+        """Метод возвращает цену продукта"""
         return self.__price
 
     @price.setter
-    def prise(self, new_price: int):
+    def price(self, new_price):
+        """Метод позволяет изменить цену продукта"""
         if new_price <= 0:
-            raise ValueError("Price must be positive")
+            print("Цена не должна быть нулевая или отрицательная")
+        elif new_price < self.__price:
+            confirmation = input(
+                f"Подтвердите понижение цены c {self.__price} до {new_price}: y(да)/n(нет)\n"
+            ).lower()
+            if confirmation == "y":
+                self.__price = new_price
+        else:
             self.__price = new_price
-        self.__price = new_price
-
-
-product1 = Product("Product 1", "Description 1", 99.99, 10)
-product2 = Product("Product 2", "Description 2", 199.99, 5)
